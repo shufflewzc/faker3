8aff914d9cbacfbae9eef696b842f37f5a606e8c
Faker <Faker@apple.com>
2022-03-30 13:19:42 +0800

    sync

--- jd_identicalnew.py
+++ jd_identicalnew.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 """
 cron: 50 * * * *
-new Env('禁用重复任务青龙2.11版本');
+new Env('禁用重复任务青龙2.12版本');
 """
 
 import json
@@ -28,7 +28,7 @@ if not ipport:
     ipport = "localhost:5700"
 else:
     ipport = ipport.lstrip("http://").rstrip("/")
-sub_str = os.getenv("RES_SUB", "shufflewzc_faker3")
+sub_str = os.getenv("RES_SUB", "shufflewzc_faker2")
 sub_list = sub_str.split("&")
 res_only = os.getenv("RES_ONLY", True)
 headers = {
@@ -155,6 +155,9 @@ def disable_duplicate_tasks(ids: list) -> None:
 
 def get_token() -> str or None:
     try:
+        with open("/ql/data/config/auth.json", "r", encoding="utf-8") as f:
+            data = json.load(f)
+    except FileNotFoundError:
         with open("/ql/config/auth.json", "r", encoding="utf-8") as f:
             data = json.load(f)
     except Exception:
